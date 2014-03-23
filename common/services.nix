0ed9ab096e3ddfb5f47bc168ab7a7e2d7bbf3726
vi <noreply@example.org>
2014-03-24 02:05:07 +0800

    acpid: power down if the lid is closed and we aren't charging

--- common/services.nix
+++ common/services.nix
@@ -3,7 +3,15 @@
 {
   services.openssh.enable = false;
   services.printing.enable = false;
-  services.acpid.enable = true;
+
+  services.acpid = {
+    enable = true;
+    lidEventCommands = ''
+      lidClosed()  { grep -q closed /proc/acpi/button/lid/LID*/state; };
+      isCharging() { grep -qiP '(?<!dis)charging' /sys/class/power_supply/BAT*/status; }
+      if `lidClosed`; then isCharging || poweroff; fi
+    '';
+  };
 
   services.xserver = {
     enable = true;
