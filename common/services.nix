c3d82fa6dce278349bd147677d7291d29f345ace
vi <noreply@example.org>
2014-03-24 02:27:46 +0800

    Slight delay before acpid checks; waiting for state update.

--- common/services.nix
+++ common/services.nix
@@ -9,15 +9,16 @@
     lidEventCommands = ''
       lidClosed()  { grep -q closed /proc/acpi/button/lid/LID*/state; };
       isCharging() { grep -qiP '(?<!dis)charging' /sys/class/power_supply/BAT*/status; }
-      if `lidClosed`; then isCharging || poweroff; fi
+      sleep 2; if `lidClosed`; then isCharging || poweroff; fi
     '';
     acEventCommands = ''
-      isCharging() { grep -qiP '(?<!dis)charging' /sys/class/power_supply/BAT*/status; }
-      isCharging || echo -n 120 > /sys/class/backlight/intel_backlight/brightness
+      isCharging()    { grep -qiP '(?<!dis)charging' /sys/class/power_supply/BAT*/status; }
+      setBrightness() { echo -n $1 > /sys/class/backlight/intel_backlight/brightness; }
+      sleep 2; isCharging && setBrightness 200 || setBrightness 120
     '';
   };
 
-  services.xserver = {
+  services.xserver = { 
     enable = true;
     exportConfiguration = true;
     xkbOptions = "ctrl:nocaps,compose:rwin";
