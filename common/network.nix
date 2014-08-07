16e932d1877ba5e63aa2c32fb162fefb9ac15ccb
vi <me@vikramverma.com>
2014-08-07 17:36:03 +0800

    Remove host mappings to Eris deployment until I start using them.

--- common/network.nix
+++ common/network.nix
@@ -2,8 +2,8 @@
 
 {
   networking.extraHosts = ''
-    192.168.56.101  eris.zalora.sg
-    192.168.56.101  bob.eris.zalora.sg
+    # 192.168.56.101  eris.zalora.sg
+    # 192.168.56.101  bob.eris.zalora.sg
   ''; 
 
   networking.enableIPv6 = false;
