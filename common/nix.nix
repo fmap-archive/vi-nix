bae3efb534a0c1b899e0db6027d01e8d06eb3162
vi <me@vikramverma.com>
2014-08-30 01:15:27 +0800

    Distribute builds to bitlbee!

--- common/nix.nix
+++ common/nix.nix
@@ -2,6 +2,17 @@
 
 {
   nixpkgs.config.allowUnfree = true;
+
+ nix.buildMachines =
+   [ { hostName = "bitlbee.ops.zalora.com";
+       sshUser  = "vi";
+       sshKey   = "/home/vi/.ssh/id_bitlbee";
+       system   = "x86_64-linux";
+       maxJobs  = 4;
+     }
+   ];
+  
+  nix.distributedBuilds = true;
   
   nix.binaryCaches = 
     [ "https://hydra.nixos.org"
