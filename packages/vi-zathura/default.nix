96bdf02b8c71ec80ff605d1ddf2280424ad4043b
vi <me@vikramverma.com>
2014-10-20 04:12:38 +0800

    Fixed vi-zathura derivation :-P.

--- packages/vi-zathura/default.nix
+++ packages/vi-zathura/default.nix
@@ -3,9 +3,11 @@
 let
   inherit (lib) overrideDerivation;
   inherit (pkgs) fetchurl;
-in overrideDerivation pkgs.zathura (_: {
-  src = fetchurl {
-    url = "https://github.com/fmap/zathura-vi/releases/download/vi-0.2.8/zathura-0.2.7.tar.gz";
-    sha256 = "8cb6553f67c4e53e23f11a2d83c19bc41fcf0c15933d70e801c598c17480dbd2";
-  };
+in overrideDerivation pkgs.zathuraCollection.zathuraWrapper (_: {
+  zathura_core = overrideDerivation pkgs.zathuraCollection.zathuraWrapper.zathura_core (_ : {
+    src = fetchurl {
+      url = "https://github.com/fmap/zathura-vi/releases/download/vi-0.2.8/zathura-0.2.7.tar.gz";
+      sha256 = "8cb6553f67c4e53e23f11a2d83c19bc41fcf0c15933d70e801c598c17480dbd2";
+    }; 
+  });
 })
