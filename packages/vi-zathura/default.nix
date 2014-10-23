f2590cc671d9285b633574dc8794c7c94afbcad7
vi <me@vikramverma.com>
2014-10-23 12:40:31 +0000

    Updated zathura-vi.

--- packages/vi-zathura/default.nix
+++ packages/vi-zathura/default.nix
@@ -2,12 +2,14 @@
 
 let
   inherit (lib) overrideDerivation;
-  inherit (pkgs) fetchurl;
+  inherit (pkgs) fetchgit python27Packages;
 in overrideDerivation pkgs.zathuraCollection.zathuraWrapper (_: {
   zathura_core = overrideDerivation pkgs.zathuraCollection.zathuraWrapper.zathura_core (_ : {
-    src = fetchurl {
-      url = "https://github.com/fmap/zathura-vi/releases/download/vi-0.2.8/zathura-0.2.7.tar.gz";
-      sha256 = "8cb6553f67c4e53e23f11a2d83c19bc41fcf0c15933d70e801c598c17480dbd2";
-    }; 
+    src = fetchgit {
+      url    = https://github.com/fmap/zathura-vi;
+      rev    = "e86077de8a9950fb72a9935772aa3b6eba70e97e";
+      sha256 = "fa2af0b2364cf172bc906afa01ba0a0d36f7c42957ecdb702d3e03faec0673cc";
+    };
+    buildInputs = [python27Packages.sphinx];
   });
 })
