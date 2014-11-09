c44aebf5c499f44f9a828501302203d6f07d3fa3
vi <me@vikramverma.com>
2014-11-09 11:54:53 +0000

    Network: drop extraHosts, running all Eris builds on EC2.

--- network.nix
+++ network.nix
@@ -1,12 +1,6 @@
 { config, lib, pkgs, ... }:
 
-{
-  networking.extraHosts = ''
-    # 192.168.56.101  eris.zalora.sg
-    # 192.168.56.101  bob.eris.zalora.sg
-  '';
-
-  networking.enableIPv6 = false;
+{ networking.enableIPv6 = false;
 
   networking.useTorAsTransparentProxy = !config.environment.isServer;
 }
