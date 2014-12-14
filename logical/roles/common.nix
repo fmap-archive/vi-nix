c105de9567f4cd3224ef0c23bc8fd10aed5d9f6a
vi <me@vikramverma.com>
2014-12-14 13:04:14 +0000

    Avoid redundant specification of root's SSH keys.

--- logical/roles/common.nix
+++ logical/roles/common.nix
@@ -9,7 +9,7 @@
 
   users.extraUsers.root = {
     hashedPassword = "$6$4CptotwIgzT58jB$CIhWW6FzHO1irNchO.KYaLBwKZLrknoMPzWVNjfJH9FQf0opTrJmzguV6lblT7z4dDPNrZoN07GpoZThzwY960";
-    openssh.authorizedKeys.keys = ["ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAAnko8n4kcVcreZO62HdQ/s9ysRHrCwUKc17f7aITwSOmwJUDmmCBrODYvkIr822kgrG0dykfrIjSa1hi0xtCjFEACreAX0N2oiocqrAgeBfbQnGjeziDoqDpOUdI1rdiMK3XN3ZTvZsRFe1HO0vDv/JkfaV4+gnzBxwXW2jgkebKI/6w==" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6EXMZwI6ng1irYcl8CBkj4X7gSj23BTbA0N7gHPHz7pNOeF0MkTa7Yx0iqS2nOjMb6jvaW6T0GBZSLL0f6rpaiABA0cZEArZ/O408QROZxK5wiR7HtO3NZsCKrTR37neWkDaNhUuL08Q6PrVDQOj14aFRa290WRXmCesTuEsK0PMxWjOtZZsqaFq5czl6grcnK1L5tDEeLU7V6xlsy/49WCKySSGu9+Le0Ao/K0Qr3vvssUdF/xWGk6ipo3DFxkiAR42NSB4SpuKuieYBN+bPFtpbgIfoZqUNMS3RFV/RU3EsZu2QmVjc5STbNHscKBsKQbe/+wUMzgd2NbYcHfst"];
+    openssh.authorizedKeys.keys = config.users.extraUsers.vi.openssh.authorizedKeys.keys;
   };
 
   users.extraUsers.vi = {
