0029141567d01e1f55ce70bfe6934daa2d4ef2b3
vi <me@vikramverma.com>
2015-01-15 11:30:16 +0000

    Provide PostgreSQL in development environments, both it and MySQL started on-demand.

--- logical/roles/development.nix
+++ logical/roles/development.nix
@@ -6,4 +6,9 @@
 
   services.mysql.enable = true;
   services.mysql.package = pkgs.mysql;
+  systemd.services.mysql.wantedBy = pkgs.lib.mkForce [];
+
+  services.postgresql.enable = true;
+  services.postgresql.package = pkgs.postgresql;
+  systemd.services.postgresql.wantedBy = pkgs.lib.mkForce [];
 }
