6cc1a61b3eefa90c067744ca5859aa4e865168af
vi <me@vikramverma.com>
2015-02-19 12:51:34 +0000

    rotated and encrypted (hashed) passwords

--- logical/roles/common.nix
+++ logical/roles/common.nix
@@ -17,7 +17,7 @@
     extraGroups = ["wheel" "vboxusers" ];
     group = "users";
     home = "/home/vi";
-    hashedPassword = "$6$jsl6DJ3J$p/45DOLzK5frmUNKonpcsWx5f.cQuz6Su6wjBftoQnM7DHzdTo/6Lh0OaV1E7Jn4dp4OrzaepjACz/zZqbCIP0";
+    hashedPassword = builtins.readFile <vi-nix/secrets/hashed-password.vi>;
     useDefaultShell = true;
     uid = 1337;
     openssh.authorizedKeys.keys = ["ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAAnko8n4kcVcreZO62HdQ/s9ysRHrCwUKc17f7aITwSOmwJUDmmCBrODYvkIr822kgrG0dykfrIjSa1hi0xtCjFEACreAX0N2oiocqrAgeBfbQnGjeziDoqDpOUdI1rdiMK3XN3ZTvZsRFe1HO0vDv/JkfaV4+gnzBxwXW2jgkebKI/6w==" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6EXMZwI6ng1irYcl8CBkj4X7gSj23BTbA0N7gHPHz7pNOeF0MkTa7Yx0iqS2nOjMb6jvaW6T0GBZSLL0f6rpaiABA0cZEArZ/O408QROZxK5wiR7HtO3NZsCKrTR37neWkDaNhUuL08Q6PrVDQOj14aFRa290WRXmCesTuEsK0PMxWjOtZZsqaFq5czl6grcnK1L5tDEeLU7V6xlsy/49WCKySSGu9+Le0Ao/K0Qr3vvssUdF/xWGk6ipo3DFxkiAR42NSB4SpuKuieYBN+bPFtpbgIfoZqUNMS3RFV/RU3EsZu2QmVjc5STbNHscKBsKQbe/+wUMzgd2NbYcHfst"];
