224b4c48df79badb3579422e7634f7dee9f3058c
vi <me@vikramverma.com>
2015-05-01 21:44:01 +0000

    euclid's an osx machine, now

--- network.nix
+++ network.nix
@@ -8,10 +8,6 @@
     imports = [ <vi-nix/physical/eee1000h.nix> <vi-nix/logical/birkhoff.nix> ]; 
   };
   
-  euclid = { 
-    imports = [ <vi-nix/physical/mbp.nix> <vi-nix/logical/euclid.nix> ];
-  };
-
 # lang = {
 #   imports = [ <vi-nix/physical/dc3217iye.nix> <vi-nix/logical/lang.nix> ];
 # };
